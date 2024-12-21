bc5b989c890e8e88a41b3e039990cabb3dc4e3ff
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-21 12:58:05 -0300

    chore: add shadcn

---
+++ frontend/app/components/DashboardSidebar.tsx
@@ -0,0 +1,76 @@
+import Link from 'next/link'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from '@/components/ui/sidebar'
+
+import {
+  ArrowLeft,
+  LayoutDashboard,
+  FileVideo
+} from 'lucide-react'
+
+// Menu items.
+const items = [
+  {
+    title: 'Dashboard',
+    url: '/dashboard',
+    icon: LayoutDashboard
+  },
+  {
+    title: 'Create new',
+    url: '/create-new',
+    icon: FileVideo
+  },
+]
+
+export default function DashboardSidebar() {
+  return (
+    <Sidebar collapsible='icon' variant='inset'>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href='/' className='text-sky-700 hover:text-sky-600'>
+                <ArrowLeft />
+                <span>Back to site</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarRail />
+    </Sidebar>
+  )
+}
