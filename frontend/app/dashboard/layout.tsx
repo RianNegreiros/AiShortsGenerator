83213744ef237a683cb5fb44c97eed6899785bd8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-21 12:59:01 -0300

    feat: add dashboard(#1)
    closes #1

---
+++ frontend/app/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger
+} from '@/components/ui/sidebar'
+import { cookies } from 'next/headers'
+import DashboardSidebar from '../components/DashboardSidebar'
+
+export default async function DashboardLayout({
+  children
+}: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true'
+
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <DashboardSidebar />
+
+      <SidebarInset>
+        <main className='p-4'>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
