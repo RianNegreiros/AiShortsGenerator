61e0b71c5124346187d4897bd21f9fbc6445305b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-29 01:36:02 -0300

    refactor: skeleton card

--- frontend/app/dashboard/_components/SkeletonCard.tsx
+++ frontend/app/dashboard/_components/SkeletonCard.tsx
@@ -1,12 +1,11 @@
 import { Skeleton } from '@/components/ui/skeleton'
 
-export function SkeletonCard() {
+export function SkeletonCard({ count = 5 }) {
   return (
-    <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
-      <Skeleton className='h-[450px] w-[300px] rounded-xl' />
-      <Skeleton className='h-[450px] w-[300px] rounded-xl' />
-      <Skeleton className='h-[450px] w-[300px] rounded-xl' />
-      <Skeleton className='h-[450px] w-[300px] rounded-xl' />
+    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+      {Array.from({ length: count }).map((_, index) => (
+        <Skeleton key={index} className='h-[450px] w-[300px] rounded-xl' />
+      ))}
     </div>
   )
 }
