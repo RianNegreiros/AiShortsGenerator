48a626f5c1b48ee6f191da23a1f4700d1d8855ac
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-30 08:41:29 -0300

    feat: delete video (#27)
    closes #27

---
+++ frontend/app/api/delete-video/route.ts
@@ -0,0 +1,41 @@
+import { deleteRender } from '@remotion/lambda/client'
+import axios from 'axios'
+import { NextRequest, NextResponse } from 'next/server'
+
+const bucketName = process.env.REMOTION_AWS_BUCKET_NAME
+
+export async function DELETE(req: NextRequest) {
+  if (!bucketName) {
+    return NextResponse.json(
+      { error: 'BucketName not defined' },
+      { status: 500 },
+    )
+  }
+
+  const { renderId, videoId } = await req.json()
+
+  console.log(renderId)
+
+  try {
+    if (renderId) {
+      await deleteRender({
+        bucketName,
+        region: 'us-east-1',
+        renderId,
+      })
+    }
+
+    await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/videos/${videoId}`)
+
+    return NextResponse.json(
+      { message: 'Video deleted successfully' },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error('Error deleting video:', error)
+    return NextResponse.json(
+      { error: 'Failed to delete video', details: error },
+      { status: 500 },
+    )
+  }
+}
